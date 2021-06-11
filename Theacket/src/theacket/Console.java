a191b1e17c3b5dad2edfdb84c1bb6ab84cb38f0b
galdinodeivid <galdinodeivid@gmail.com>
2021-06-11 18:41:47 -0300

    .

--- Theacket/src/theacket/Console.java
+++ Theacket/src/theacket/Console.java
@@ -7,33 +7,45 @@ import java.util.Scanner;
 
 public class Console {
 
+	public static Scanner userInput = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
-		System.out.println(getLogo() + "\n" + getComandosModo());
-        Scanner input = new Scanner(System.in);
-        String comando;
-        
+		System.out.println(getLogo() + "\n" + getModos());
+		String modo;
+      
         do {
-        	System.out.print(colorize("modo", GREEN_TEXT()) + "@" + colorize("Theacket", BLUE_TEXT()) + "~$ ");
-        	comando = input.nextLine();
+        	System.out.print(colorize("[modo]", YELLOW_TEXT()) + " Informe em qual modo deseja entrar: ");
+        	modo = userInput.next();
         	
-        	switch (comando) {
-        	case "sair":
-        		System.out.println("Saindo do programa!");
-        		break;
-        	case "cli":
-        		System.out.println("Entrando no modo cliente!");
-        		break;
-        	case "adm":
-        		System.out.println("Entrando no modo adm!");
-        		break;
-        	default:
-        		erroMes("Comando \"" + comando + "\" inválido!");
+        	if (!modo.equals("cli") && !modo.equals("adm") && !modo.equals("sair")) {
+        		erroMes("Modo \"" + modo + "\" inválido!");
         	}
         	
-        } while (!comando.equals("sair"));
+        } while (!modo.equals("cli") && !modo.equals("adm") && !modo.equals("sair"));
         
-        
+        if (!modo.equals("sair")) {
+        	String comando;
+        	
+        	do {
+        		System.out.print(colorize("modo", GREEN_TEXT()) + "@" + colorize("Theacket", BLUE_TEXT()) + "~$ ");
+        		comando = userInput.next();
+        		switch (comando) {
+        		case "sair":
+        			System.out.println("Saindo do programa!");
+        			break;
+        		case "cli":
+        			System.out.println("Entrando no modo cliente!");
+        			break;
+        		case "adm":
+        			System.out.println("Entrando no modo adm!");
+        			break;
+        		default:
+        			erroMes("Comando \"" + comando + "\" inválido!");
+        		}
+        		
+        	} while (!comando.equals("sair"));
+        }
 	}
 	
 	public static String getLogo() {
@@ -45,6 +57,24 @@ public class Console {
 				"  | |  | | | ||  __/| (_| || (__ |   <|  __/| |_ \n" +
 				"  \\_/  |_| |_| \\___| \\__,_| \\___||_|\\_\\\\___| \\__|\n";
 	    return logo;
+	}
+	
+	public static String getModos() {
+		String text = "#Modos:\n" +
+				"cli     entrar no modo cliente\n" +
+				"adm     entrar no modo administrativo\n" +
+				"sair    sair do programa\n";
+		return text;
+	}
+	
+	public static int validaEntradaInt(String mensagem) {
+		int input;
+		userInput.next();
+		while (!userInput.hasNextInt()) {
+			erroMes(mensagem);
+		}
+		input = userInput.nextInt();
+		return input;
 		
 	}
 	
@@ -52,12 +82,5 @@ public class Console {
 		System.out.println(colorize("[ERRO] " + texto, RED_TEXT()));
 	}
 	
-	public static String getComandosModo() {
-		String text = "Comandos:\n" +
-				"cli     entrar no modo cliente\n" +
-				"adm     entrar no modo administrativo\n" +
-				"sair    sair do programa\n";
-	    return text;
-	}
 
 }
